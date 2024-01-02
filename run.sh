49ef501204619447a3b55ccdc4c524f832761db3
Muhammad Fikri <40158214+xynogen@users.noreply.github.com>
2024-01-03 04:36:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-
-
 /home/xynogen/Apps/anaconda3/envs/PAIO/bin/python3.8 BasicFirebase.py &
 /home/xynogen/Apps/anaconda3/envs/PAIO/bin/python3.8 BasicFirebase2.py &
 /home/xynogen/Apps/anaconda3/envs/PAIO/bin/python3.8 BasicFirebase3.py &
-
